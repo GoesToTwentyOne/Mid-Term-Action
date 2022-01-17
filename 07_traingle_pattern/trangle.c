c6c20b248f2fd8c88b7913fd0c02bc20ad1de399
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-01-17 17:33:32 +0600

    trangle

---
+++ 07_traingle_pattern/trangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  int n;
+  printf("Enter star limit :" );
+  scanf("%d", &n);
+  for(int row=1;row<=n;row++){
+    for(int col=1;col<=n-row;col++){
+      printf(" ");
+    }
+    for(int col=1;col<=2*row-1;col++){
+      printf("*");
+    }
+    printf("\n");
+  }
+}
